5080d1457d3ce3541abe20219f65c44bfb13983f
pieisland <pieisland@naver.com>
2019-01-11 11:35:14 +0900

    그래픽 카드 설치 편의용

---
+++ scripts/nvidia.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#It it a shell script for installing a graphic card driver.
+yum -y update
+yum -y install kernel-devel
+yum -y install kernel-headers
+yum -y install gcc
+yum -y install make
+
+#모팩아카데미용 그래픽 카드 드라이버
+cd /tmp
+wget http://us.download.nvidia.com/XFree86/Linux-x86_64/410.93/NVIDIA-Linux-x86_64-410.93.run
