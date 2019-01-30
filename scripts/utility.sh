773ecf8e2b69b7a1ef4ffb0515ffd9b8831a5bd6
pieisland <pieisland@naver.com>
2019-01-30 15:16:29 +0900

    ImageMagick

--- scripts/utility.sh
+++ scripts/utility.sh
@@ -7,7 +7,8 @@ yum -y install mesa-libGLU # for Nuke and other Graphics SW
 yum -y install transmission
 yum -y install tigervnc-server
 yum -y install filezilla
-yum -t install tree
+yum -y install tree
+yum -y install ImageMagick
 
 #ripgrep
 yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
