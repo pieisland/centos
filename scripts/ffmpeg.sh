e2021ca9b49f2aaa37dc3238ce99eabb1229243a
pieisland <pieisland@naver.com>
2019-01-31 14:19:20 +0900

    ffmpeg

---
+++ scripts/ffmpeg.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [! -d "/home/$USER/app" ]; then
+	mkdir /home/$USER/app	
+fi
+
+if [! -d "/home/$USER/app/ffmpeg"]; then
+	mkdir /home/$USER/app/ffmpeg
+
+cd /home/$USESR/app/ffmpeg
+
+wget http://johnvansickle.com/ffmpeg/builds/ffmpeg-git-64bit-static.tar.xz
+tar xpvf ffmpeg-git-64bit-static.tar.xz --strip 1
