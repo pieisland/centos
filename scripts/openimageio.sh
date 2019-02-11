110a73eb1dcb92a362a13e791a0b12951a7ef5f4
namgwang <pieisland@naver.com>
2019-02-11 14:38:32 +0900

    open color io

--- scripts/openimageio.sh
+++ scripts/openimageio.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+if [[$EUID -ne 0]]; then
+	echo "error" 1>&2
+	exit 1
+fi
+
+yum -y install OpenColorIO
 yum -y install OpenImageIO
 yum -y install OpenImageIO-iv
 yum -y install OpenImageIO-devel
