405bf3938bbf7ff1620c73922412eeaa912acdf1
namgwang <pieisland@naver.com>
2019-02-12 10:43:03 +0900

    pip

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -14,3 +14,7 @@ pip install --user xlrd
 pip install --user xlwt
 pip install --user xlutils
 pip install --user pillow
+pip install --user timecode
+pip install --user edl
+pip install --user PySide2
+pip install --user opentimelineio
