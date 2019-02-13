0837613bc3e593e1f50ec732f7497c61ceb0dc6d
namgwang <pieisland@naver.com>
2019-02-13 11:18:23 +0900

    gaffer and blender

---
+++ scripts/gaffer.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ ! -d "/home/$USER/app"]; then
+	mkdir /home/$USER/app
+fi
+
+#IlmBase compile
+cd /home/$USER/app
+wget https://github.com/ImageEngine/gaffer/releases/download/0.53.0.0/gaffer-0.53.0.0-linux.tar.gz
+tar -zxvf gaffet-0.53.0.0-linux.tar.gz
