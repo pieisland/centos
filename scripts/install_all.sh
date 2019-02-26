dd38a2382844490f948d02542cd92932334e7f19
namgwang <pieisland@naver.com>
2019-02-26 15:28:12 +0900

    correcting bug

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -1,30 +1,34 @@
 #!/bin/sh
 
-if [[$EUID -ne 0]]; then
-    ./root/nvidia.sh # 
-    ./root/utility.sh #
-    ./root/code.sh #
-    ./root/djv.sh #
-    ./root/mpv.sh #
-    ./root/devtools.sh #
-    ./root/handbrake.sh #
-    ./root/openimageio.sh #
-    ./root/rmhotcorner.sh #
+if [[$EUID -e 0]]; then
+    ./root/nvidia.sh  
+    ./root/utility.sh 
+    ./root/code.sh 
+    ./root/djv.sh 
+    ./root/mpv.sh
+    ./root/devtools.sh
+    ./root/handbrake.sh
+    ./root/openimageio.sh
+    ./root/rmhotcorner.sh
+	./rmbeep.sh
+else
 
-# install $USER
-./rmbeep.sh
-./user/trans.sh
-./user/ffmpeg.sh
-./user/ocio_config.sh
-./user/alembic.sh
-./user/pip.sh
-./user/opencolorio.sh
-./user/blender.sh
-./user/cmake.sh
-./user/gaffer.sh
-./user/openexr.sh
+	# install $USER
+	./rmbeep.sh
+	./user/trans.sh
+	./user/ffmpeg.sh
+	./user/ocio_config.sh
+	./user/alembic.sh
+	./user/pip.sh
+	./user/opencolorio.sh
+	./user/blender.sh
+	./user/cmake.sh
+	./user/gaffer.sh
+	./user/openexr.sh
 
-#By using gcc6.x, do build.
-scl enable devtoolset -6 ./cmake.sh
-scl enable devtoolset -6 ./openexr.sh
-scl enable devtoolset -6 ./opencolorio.sh
+	#By using gcc6.x, do build.
+	scl enable devtoolset -6 ./user/cmake.sh
+	scl enable devtoolset -6 ./user/openexr.sh
+	scl enable devtoolset -6 ./user/opencolorio.sh
+	scl enable devtoolset -6 ./user/alembic.sh
+fi
