479206b77af0a7090992379af23d9cbc75c3d730
namgwang <pieisland@naver.com>
2019-02-26 12:25:23 +0900

    database shell

---
+++ scripts/mongodb.sh
@@ -0,0 +1,10 @@
+echo "[mongodb-org-4.0]" >> /etc/yum.repos.d/mongodb-org.repo
+echo "name=MongoDB Repository"
+echo "baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/4.0/x86_64/"
+echo "gpgcheck=1"
+echo "enabled=1"
+echo "gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc"
+
+yum install -y mongodb-org
+firewall-cmd --zone=public --add-port=27017/tcp --permanent
+firewall-cmd --reload
---
+++ scripts/redis.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+yum -y install redis
+systemctl enable redis
+systemctl start redis
+
+firewall-cmd --zone=public --add-port=6379/tcp --permanent
+firewall-cmd --reload
+
+
